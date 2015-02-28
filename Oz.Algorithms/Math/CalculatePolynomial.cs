ba7c80563080b4d107acc21085fe1c9ed29c7cb6
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-03-01 00:47:55 +0200

    Implementation of CalculatePolynomial Method which gives value of a polynomial at given x.

---
+++ Oz.Algorithms/Math/CalculatePolynomial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Oz.Algorithms.Math
+{
+    /// <summary>
+    /// Calculates result of Polynomial At Given 'x' value
+    /// </summary>
+    public class CalculatePolynomial
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CalculatePolynomial()
+        {
+            
+        }
+
+        public double calculate(List<double> _inputPolynomial, double x)
+        {
+            double result = 0.0;
+            int size = _inputPolynomial.Count;
+            foreach (var val in _inputPolynomial)
+            {
+                if(--size == 0){
+                    result += val;
+                }
+                else{
+                    result += (val * System.Math.Pow(x,size));
+                }                
+            }
+            return result;
+        }
+    }
+}
