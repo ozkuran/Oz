a0b3d7fc47a7f4e84a80c717f6cbe564a04a3aa1
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-02-28 03:19:06 +0200

    Added Path Object

---
+++ Oz.Algorithms/Math/Graph/Path.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oz.Algorithms.Math.Graph
+{
+    public class Path
+    {
+        private List<Vertex> Pathway { get; set; }
+        private List<Double> Pathlength { get; set; }
+
+        public Path()
+        {
+            
+        }
+
+    }
+}
