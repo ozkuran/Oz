8289dd526384011731eb33bc6257ec4728f08ea3
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-02-28 03:18:15 +0200

    Implemented Dijkstra's Shortest Path Algorithm in Graph Class.

--- Oz.Algorithms/Math/Graph/Graph.cs
+++ Oz.Algorithms/Math/Graph/Graph.cs
@@ -1,28 +1,178 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using JetBrains.Annotations;
 
 namespace Oz.Algorithms.Math.Graph
 {
+    /// <summary>
+    /// Graph Class
+    /// </summary>
     public class Graph
     {
+        /// <summary>
+        /// Id of the Graph
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Name of the Graph
+        /// </summary>
         public string Name { get; set; }
-        public List<Node> Nodes { get; set; }
+        /// <summary>
+        /// Vertexes of the Graph
+        /// </summary>
+        public List<Vertex> Vertexes { get; set; }
+        /// <summary>
+        /// Edges of the Graph
+        /// </summary>
         public List<Edge> Edges { get; set; }
-        private int TotalEdgeWeight { get; set; }
+        /// <summary>
+        /// Total Edge Weight of the Graph
+        /// </summary>
+        public int TotalEdgeWeight { get; set; }
+        /// <summary>
+        /// Maximum Edge Count of a Vertex
+        /// Useful Metric in calculating Density of Graph
+        /// </summary>
         public int MaximumEdgeCount { get; set; }
+        /// <summary>
+        /// Density of the Graph
+        /// </summary>
         public double Density { get; set; }
+        /// <summary>
+        /// Weight of the Biggest Edge in the Graph
+        /// </summary>
         public int BiggestEdgeWeight { get; set; }
+        /// <summary>
+        /// Distance Matrix of the Graphs shortest paths
+        /// Calculations made with normalized weights of edges
+        /// </summary>
+        public List<List<double>> DistanceMatrix { get; set; } 
+        private const double Infinity = int.MaxValue;
 
 
+        /// <summary>
+        /// Constructor of Graph Class
+        /// </summary>
         public Graph()
         {
-            Nodes = new List<Node>();
+            Vertexes = new List<Vertex>();
             Edges = new List<Edge>();
+            DistanceMatrix = new List<List<double>>();
             TotalEdgeWeight = 0;
             MaximumEdgeCount = 0;
             Density = 0;
             BiggestEdgeWeight = 0;
+        }
+
+        private List<int> NeighbourVertexes(Vertex Vertex)
+        {
+            var result = new List<int>();
+            foreach (var edge in Edges)
+            {
+                if (edge.Vertex1.Id == Vertex.Id)
+                {
+                    result.Add(edge.Vertex2.Id);
+                }
+                else if (edge.Vertex2.Id == Vertex.Id)
+                {
+                    result.Add(edge.Vertex1.Id);
+                }
+            }
+            return result;
+        } 
+
+        private int GetClosestVertexesEdge(Vertex inputVertex, List<int> visitedVertexes)
+        {
+            var edgeNumber = -1;
+            foreach (var edge in Edges.Where(edge => edge.IsVertexPresent(inputVertex)))
+            {
+                if (edgeNumber == -1)
+                {
+                    edgeNumber = edge.Id;
+                }
+                else if (Edges[edgeNumber].NormalizedWeight < edge.NormalizedWeight)
+                {
+                    edgeNumber = edge.Id;
+                }
+            }
+            return edgeNumber;
+        }
+
+        /// <summary>
+        /// Calculates Shortest Path Between Given Vertexes 
+        /// </summary>
+        /// <param name="Vertex">Given Vertex</param>
+        /// <returns>Returns a list of doubles includes shortest paths to every Vertex from given Vertex</returns>
+        private double CalculateShortestPathBetweenTwoVertexes(Vertex Vertex1, Vertex Vertex2)
+        {
+            var shortestDistances = new List<double>();
+            var currentVertex = Vertex1.Id;
+            var visitedVertexes = new List<int>();
+            for (var i = 0; i < Vertexes.Count(); i++)
+            {
+                shortestDistances.Add(Infinity);
+            }
+            shortestDistances[Vertex1.Id] = 0;
+            while (!visitedVertexes.Contains(Vertex2.Id))
+            {
+                foreach (var neighbourVertex in NeighbourVertexes(Vertexes[currentVertex]))
+                {
+                    if (!visitedVertexes.Contains(GetEdgeId(Vertexes[neighbourVertex].Name, Vertexes[currentVertex].Name)))
+                    {
+                        if (System.Math.Abs(shortestDistances[neighbourVertex] - Infinity) < 10)
+                        {
+                            shortestDistances[neighbourVertex] =
+                                1 - Edges[GetEdgeId(Vertexes[neighbourVertex].Name, Vertexes[currentVertex].Name)]
+                                    .NormalizedWeight;
+                        }
+                    }
+                    visitedVertexes.Add(currentVertex);
+                    currentVertex = GetClosestVertexesEdge(Vertexes[currentVertex], visitedVertexes);
+                }
+            }
+            return shortestDistances[Vertex2.Id];
+        }
+
+        private void InitializeShortestPathMatrix()
+        {
+            for (var i = 0; i < Vertexes.Count(); i++)
+            {
+                var temporaryList = new List<double>();
+                for (var j = 0; j < Vertexes.Count(); j++)
+                {
+                    temporaryList.Add(Infinity);
+                }
+                DistanceMatrix.Add(temporaryList);
+            }
+        }
+
+        /// <summary>
+        /// Calculates Shortest Path Matrix using Dijkstra's Algorithm
+        /// </summary>
+        public void CalculateShortestPathsMatrix()
+        {
+            InitializeShortestPathMatrix();
+            for (var i = 0; i < Vertexes.Count(); i++)
+            {
+                for (var j = 0; j < Vertexes.Count(); j++)
+                {
+                    DistanceMatrix[i][j] = CalculateShortestPathBetweenTwoVertexes(Vertexes[i], Vertexes[j]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns Normalized length of an Edge between given Vertexes
+        /// </summary>
+        /// <param name="Vertex1">First Vertex</param>
+        /// <param name="Vertex2">Second Vertex</param>
+        /// <returns></returns>
+        private double GetNormalizedLengthBetweenVertexes(Vertex Vertex1, Vertex Vertex2)
+        {
+            var result = 0.0;
+            result = 1 - Edges[GetEdgeId(Vertex1.Name, Vertex2.Name)].NormalizedWeight;
+            return result;
         }
 
 
@@ -45,7 +195,7 @@ namespace Oz.Algorithms.Math.Graph
 	        {
 		        CalculateMaximumEdgeCount();
 	        }
-            Density = (double)MaximumEdgeCount / (double)Nodes.Count();
+            Density = (double)MaximumEdgeCount / (double)Vertexes.Count();
         }
 
         /// <summary>
@@ -53,25 +203,25 @@ namespace Oz.Algorithms.Math.Graph
         /// </summary>
         public void CalculateMaximumEdgeCount()
         {
-            var nodeCount = Nodes.Count;
-            MaximumEdgeCount = nodeCount * (nodeCount - 1) / 2;
+            var VertexCount = Vertexes.Count;
+            MaximumEdgeCount = VertexCount * (VertexCount - 1) / 2;
         }
 
-        public int AddNode(string nodeName)
+        public int AddVertex(string VertexName)
         {
-            if (!IsNodeAdded(nodeName))
+            if (!IsVertexAdded(VertexName))
             {
-                var node = new Node(Nodes.Count() + 1, nodeName);
-                Nodes.Add(node);
-                return Nodes.Count();
+                var Vertex = new Vertex(Vertexes.Count() , VertexName);
+                Vertexes.Add(Vertex);
+                return Vertexes.Count();
             }
             else
             {
-                var node = Nodes.FirstOrDefault(x => x.Name == nodeName);
-                if (node != null)
+                var Vertex = Vertexes.FirstOrDefault(x => x.Name == VertexName);
+                if (Vertex != null)
                 {
-                    node.Weight++;
-                    return node.Id;
+                    Vertex.Weight++;
+                    return Vertex.Id;
                 }
                 else
                 {
@@ -80,23 +230,24 @@ namespace Oz.Algorithms.Math.Graph
             }
         }
 
-        private bool IsNodeAdded(string nodeName)
+        private bool IsVertexAdded(string VertexName)
         {
-            return Nodes.Count(x => x.Name == nodeName) > 0;
+            return Vertexes.Count(x => x.Name == VertexName) > 0;
         }
 
-        private int GetNodeId(string nodeName)
+        private int GetVertexId(string VertexName)
         {
-            var node = Nodes.FirstOrDefault(x => x.Name == nodeName);
-            return node != null ? node.Id : 0;
+            var Vertex = Vertexes.FirstOrDefault(x => x.Name == VertexName);
+            return Vertex != null ? Vertex.Id : 0;
         }
+
         /// <summary>
         /// Adds Edge to Graph
         /// </summary>
         /// <param name="edge">Enge to be added to graph</param>
         public void AddEdge(Edge edge)
         {
-            var edgeId = GetEdgeId(edge.Node1.Name, edge.Node2.Name);
+            var edgeId = GetEdgeId(edge.Vertex1.Name, edge.Vertex2.Name);
             if (edgeId > 0)
             {
                 var edg = Edges.FirstOrDefault(x => x.Id == edgeId);
@@ -104,34 +255,45 @@ namespace Oz.Algorithms.Math.Graph
             }
             else
             {
-                var intNode1 = AddNode(edge.Node1.Name);
-                var intNode2 = AddNode(edge.Node2.Name);
-                edge.Node1.Id = intNode1;
-                edge.Node2.Id = intNode2;
+                var intVertex1 = AddVertex(edge.Vertex1.Name);
+                var intVertex2 = AddVertex(edge.Vertex2.Name);
+                edge.Vertex1.Id = intVertex1;
+                edge.Vertex2.Id = intVertex2;
                 edge.Weight = 1;
-                edge.Id = Edges.Count() + 1;
+                edge.Id = Edges.Count();
                 Edges.Add(edge);
             }
             TotalEdgeWeight++;
         }
 
-        // Mahmut Ali ÖZKURAN 
-        // 01/02/2015
-        // Checks if Edge already defined
-        // If not defined returns 0
-        private bool IsEdgeAdded(Edge edge)
+
+        /// <summary>
+        /// Checks if Edge already defined
+        /// If not defined returns 0
+        /// </summary>
+        /// Mahmut Ali ÖZKURAN 
+        /// 01/02/2015
+        /// <param name="edge">Edge which will be checked if added or not</param>
+        /// <returns></returns>
+        [UsedImplicitly]private bool IsEdgeAdded(Edge edge)
         {
-            var edg = Edges.FirstOrDefault(x => ((x.Node1.Name == edge.Node1.Name) && (x.Node2.Name == edge.Node2.Name)));
+            var edg = Edges.FirstOrDefault(x => ((x.Vertex1.Name == edge.Vertex1.Name) && (x.Vertex2.Name == edge.Vertex2.Name)));
             return edg != null;
         }
 
-        // Mahmut Ali ÖZKURAN 
-        // 01/02/2015
-        // Returns id of Egde if already defined
-        // If not defined returns 0
-        private int GetEdgeId(string node1, string node2)
+        /// <summary>
+        /// Returns id of Egde if already defined
+        /// If not defined returns 0
+        /// </summary>
+        /// Mahmut Ali ÖZKURAN 
+        /// 01/02/2015
+        /// <param name="Vertex1">First Vertex to check</param>
+        /// <param name="Vertex2">Second Vertex to check</param>
+        /// <returns></returns>
+        private int GetEdgeId(string Vertex1, string Vertex2)
         {
-            var edge = Edges.FirstOrDefault(x => ((x.Node1.Name == node1) && (x.Node2.Name == node2)));
+            var edge = Edges.FirstOrDefault(x => ((x.Vertex1.Name == Vertex1) && (x.Vertex2.Name == Vertex2))) ??
+                       Edges.FirstOrDefault(x => ((x.Vertex1.Name == Vertex2) && (x.Vertex2.Name == Vertex1)));
             return edge != null ? edge.Id : 0;
         }
 
@@ -158,15 +320,15 @@ namespace Oz.Algorithms.Math.Graph
         public void SaveAsGdf(string filename)
         {
             var file = new System.IO.StreamWriter(@"Data\\" + filename + ".gdf");
-            file.WriteLine("nodedef>name VARCHAR,weight DOUBLE");
-            foreach (var node in Nodes)
+            file.WriteLine("Vertexdef>name VARCHAR,weight DOUBLE");
+            foreach (var Vertex in Vertexes)
             {
-                file.WriteLine(node.Name + ", " + node.Weight);
+                file.WriteLine(Vertex.Name + ", " + Vertex.Weight);
             }
-            file.WriteLine("edgedef>node1 VARCHAR,node2 VARCHAR,weight DOUBLE");
+            file.WriteLine("edgedef>Vertex1 VARCHAR,Vertex2 VARCHAR,weight DOUBLE");
             foreach (var edge in Edges)
             {
-                file.WriteLine(edge.Node1.Name + ", " + edge.Node2.Name + ", " + edge.Weight);
+                file.WriteLine(edge.Vertex1.Name + ", " + edge.Vertex2.Name + ", " + edge.Weight);
             }
             file.Close();
         }
