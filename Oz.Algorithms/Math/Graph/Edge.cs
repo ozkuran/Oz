1ebeca1f2a842595497bc998e3ae33d9c73cb83f
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2015-02-28 03:16:55 +0200

    Refactor

--- Oz.Algorithms/Math/Graph/Edge.cs
+++ Oz.Algorithms/Math/Graph/Edge.cs
@@ -5,8 +5,8 @@ namespace Oz.Algorithms.Math.Graph
     public class Edge
     {
         public int Id { get; set; }
-        public Node Node1 { get; set; }
-        public Node Node2 { get; set; }
+        public Vertex Vertex1 { get; set; }
+        public Vertex Vertex2 { get; set; }
         public int Weight { get; set; }
         public double NormalizedWeight { get; set; }
 
@@ -16,52 +16,52 @@ namespace Oz.Algorithms.Math.Graph
             Weight = 0;
         }
 
-        public Edge(Node node1, Node node2)
+        public Edge(Vertex vertex1, Vertex vertex2)
         {
             Id = 0;
-            Node1 = node1;
-            Node2 = node2;
+            Vertex1 = vertex1;
+            Vertex2 = vertex2;
             Weight = 1;
             NormalizedWeight = 0;
         }
 
-        public Edge(int id, Node node1, Node node2)
+        public Edge(int id, Vertex vertex1, Vertex vertex2)
         {
             Id = id;
-            Node1 = node1;
-            Node2 = node2;
+            Vertex1 = vertex1;
+            Vertex2 = vertex2;
             Weight = 1;
             NormalizedWeight = 0;
         }
 
         /// <summary>
-        /// Returns true if given Node present in Edge
+        /// Returns true if given Vertex present in Edge
         /// </summary>
-        /// <param name="node"></param>
+        /// <param name="vertex"></param>
         /// <returns></returns>
-        public bool IsNodePresent(Node node)
+        public bool IsVertexPresent(Vertex vertex)
         {
-            var result = Node1.Id == node.Id || Node2.Id == node.Id;
+            var result = Vertex1.Id == vertex.Id || Vertex2.Id == vertex.Id;
             return result;
         }
 
 
         /// <summary>
-        /// Returns the node of edge other node than given node
-        /// If given node not in edge returns -1
+        /// Returns the vertex of edge other vertex than given vertex
+        /// If given vertex not in edge returns -1
         /// </summary>
-        /// <param name="node"></param>
+        /// <param name="vertex"></param>
         /// <returns></returns>
-        public int GetOtherNode(Node node)
+        public int GetOtherVertex(Vertex vertex)
         {
             var result = -1;
-            if (Node1.Id == node.Id)
+            if (Vertex1.Id == vertex.Id)
             {
-                result = Node2.Id;
+                result = Vertex2.Id;
             }
-            else if (Node2.Id == node.Id)
+            else if (Vertex2.Id == vertex.Id)
             {
-                result = Node2.Id;
+                result = Vertex2.Id;
             }
             return result;
         }
