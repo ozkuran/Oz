d2fd429e4c2e83580c0eff0c1ceba9bed9dde45d
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-10-19 15:06:24 +0300

    #BugFix

--- Oz.Algorithms.Tests/TwitterTests.cs
+++ Oz.Algorithms.Tests/TwitterTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Oz.Algorithms.Data.Download.Twitter;
+//using Oz.Algorithms.Data.Download.Twitter;
 using System.Configuration;
 using System.IO;
 
@@ -13,16 +13,16 @@ namespace Oz.Algorithms.Tests
     [TestClass]
     public class TwitterTests
     {
-        private Twitter _twitter;
+        //private Twitter _twitter;
 
-        public TwitterTests()
-        {
-            string twitterConsumerKey = ConfigurationManager.AppSettings["twitterConsumerKey"];
-            string twitterConsumerSecret = ConfigurationManager.AppSettings["twitterConsumerSecret"];
-            string twitterAccessToken = ConfigurationManager.AppSettings["twitterAccessToken"];
-            string twitterAccessTokenSecret = ConfigurationManager.AppSettings["twitterAccessTokenSecret"];
-            _twitter = new Twitter(twitterConsumerKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret);
-        }
+        //public TwitterTests()
+        //{
+        //    string twitterConsumerKey = ConfigurationManager.AppSettings["twitterConsumerKey"];
+        //    string twitterConsumerSecret = ConfigurationManager.AppSettings["twitterConsumerSecret"];
+        //    string twitterAccessToken = ConfigurationManager.AppSettings["twitterAccessToken"];
+        //    string twitterAccessTokenSecret = ConfigurationManager.AppSettings["twitterAccessTokenSecret"];
+        //    _twitter = new Twitter(twitterConsumerKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret);
+        //}
 
         //[TestMethod]
         //public void CheckIfConnected()
